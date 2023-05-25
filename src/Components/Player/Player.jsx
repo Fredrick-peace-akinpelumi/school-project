5593fc3021f2ac5f5ada68b8c95241d33dcd61ae
Aremu Olakunle Umaru <colakunleumaru@gmail.com>
2023-05-25 03:31:35 +0100

    now the slider are working

--- src/Components/Player/Player.jsx
+++ src/Components/Player/Player.jsx
@@ -3,16 +3,61 @@ import trackImg from "../../images/trackImg.png";
 import music1 from "../../audio/wizkid1.mp3"
 import music2 from "../../audio/wizkid2.mp3"
 import "./Player.css"
-import { useRef } from "react";
+import { useCallback, useRef } from "react";
 import { useState } from "react";
 import { useEffect } from "react";
+import format from "format-duration"
 
 const Player=()=>{
     const audioRef=useRef()
+    const playAnimationRef = useRef();
+    const progressBarRef=useRef() 
     const [volume,setVolume]=useState(1)
+    const [isPlaying,setIsPlaying]=useState(false)
+    const [isShuffle, setIsShuffle]=useState(false)
+    const [duration,setDuration]=useState(0)
+    const [currentTime, setCurrentTime]=useState(0)
 
     const Play=()=>{
         audioRef.current.play()
+        setIsPlaying(true)
+    }
+
+    const repeat = useCallback(() => {
+        const audioTime = audioRef.current.currentTime;
+        setCurrentTime(audioTime);
+        //progressBarRef.current.value = currentTime;
+
+        progressBarRef.current.style.setProperty(
+            '--range-progress',
+            `${(progressBarRef.current.value / duration) * 100}%`
+        );
+        
+    
+        playAnimationRef.current = requestAnimationFrame(repeat);
+    }, [audioRef, duration, progressBarRef, setCurrentTime]);
+      
+    useEffect(() => {
+        if (isPlaying) {
+          audioRef.current.play();
+          playAnimationRef.current = requestAnimationFrame(repeat);
+        } else {
+          audioRef.current.pause();
+          cancelAnimationFrame(playAnimationRef.current);
+        }
+    }, [isPlaying, audioRef, repeat]);
+
+    
+
+    const handleProgressChange = () => {
+        audioRef.current.currentTime = progressBarRef.current.value;
+    };
+    
+    const handleLoad=()=>{
+        setDuration(audioRef.current.duration)
+        //console.log(audioRef.current?.currentTime)
+        handleChange()
+        
     }
 
     useEffect(()=>{
@@ -22,9 +67,19 @@ const Player=()=>{
         vol()
 
     },[volume])
+
+    const handleChange=useCallback(()=>{
+        console.log("changed")
+    },[audioRef.current?.currentTime])
+    
+    // useEffect(()=>{
+        
+    //     console.log("change")
+    // },[audioRef.current?.currentTime])
     
     const Pause=()=>{
         audioRef.current.pause()
+        setIsPlaying(false)
     }
     return(
         <div className="Player">
@@ -38,18 +93,30 @@ const Player=()=>{
             </div>
             <div>
                 <div>
-                    <button onClick={Play} >play</button>
-                    <button onClick={Pause}>pause</button>
+                    {isShuffle?<i className="fa notSoBig fa-random" aria-hidden="true"></i>:<i className="fa notSoBig fa-random" aria-hidden="true"></i>}
+                    <i className="fa notSoBig fa-step-backward" aria-hidden="true"></i>
+                    {isPlaying?<i onClick={Pause} className="fa bigIcon fa-pause-circle-o" aria-hidden="true"></i>:<i onClick={Play} className="fa bigIcon fa-play-circle-o" aria-hidden="true"></i>}
+                    <i className="fa notSoBig fa-step-forward" aria-hidden="true"></i>
+                    <i className="fa notSoBig fa-stop" aria-hidden="true"></i>
+
+
                 </div>
-                <audio src={music2} ref={audioRef}/>            
-                <input type="range" max="200" />
+                <audio src={music1} ref={audioRef}  onLoadedData={handleLoad} onEndedCapture={(e)=>setIsPlaying(false)}/> 
+                <div>
+                    <span>{format(1000*currentTime)}</span>
+                    <input type="range" className="music-range" value={currentTime} defaultValue={0}  onChange={handleProgressChange} ref={progressBarRef}  max={duration} />
+                    <span>{format(1000*duration)}</span>
+                </div>           
+                
             </div>
             <div>
-                <i className="bi" />
+                <i className="fa fa-volume-up" aria-hidden="true"></i>
                 <input type="range" max="8" onChange={(e)=>setVolume(e.target.value/10)}/>
             </div>
 
         </div>
     )
+
+    
 }
 export default Player
